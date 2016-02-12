72a5bffd61731c8827ca0b257b762ae557a0b87e
Matthew <mfisher@deis.com>
2016-02-11 21:52:29 -0800

    refactor Build

--- build.go
+++ build.go
@@ -1,10 +1,5 @@
 package api
 
-import (
-	"fmt"
-	"os"
-)
-
 // Build is an executable bundle of software built from source at a specific version or commit
 // specified by the deployment process.
 //
@@ -12,13 +7,16 @@ import (
 // stored in the remote registry. In future iterations, this could be a reference to a rootfs slug
 // stored directly in the controller's filesystem or on some other remote store (S3, for example).
 type Build struct {
-	// Name is the fully qualified name of the build along with the version or commit specified
+	// Image is the fully qualified name of the build along with the version or commit specified
 	// by the deployment process. In docker terms, this would be the fully-qualified docker
 	// image stored on the registry (without the remote registry URL, as this is stored in
 	// $REGISTRY_URL).
-	Name string
+	Image string
+	// Procfile is a process mapping between process types and the command associated with said
+	// process type.
+	Procfile map[string]string
 }
 
 func (b Build) String() string {
-	return fmt.Sprintf("%s/%s", os.Getenv("REGISTRY_URL"), b.Name)
+	return b.Image
 }
