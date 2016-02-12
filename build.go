f76b575b9efb861cc223ee588f516cf8bf9323ce
Matthew <mfisher@deis.com>
2016-02-11 23:12:14 -0800

    rename Image to Artifact
    This way it's a more generic term when swapping out different build
    artifacts (app slug, docker image, rkt ACI, etc.)

--- build.go
+++ build.go
@@ -7,16 +7,16 @@ package api
 // stored in the remote registry. In future iterations, this could be a reference to a rootfs slug
 // stored directly in the controller's filesystem or on some other remote store (S3, for example).
 type Build struct {
-	// Image is the fully qualified name of the build along with the version or commit specified
-	// by the deployment process. In docker terms, this would be the fully-qualified docker
-	// image stored on the registry (without the remote registry URL, as this is stored in
-	// $REGISTRY_URL).
-	Image string
-	// Procfile is a process mapping between process types and the command associated with said
-	// process type.
+	// Artifact is the fully qualified name of the build along with the version or commit
+	// specified by the deployment process. For example, with Docker this would be the
+	// fully-qualified docker image name stored on the registry. For rkt, this would be
+	// a URL to the Application Container Image (or ACI for short).
+	Artifact string
+	// Procfile is a process mapping between the artifact's process types and the command
+	// associated with said process type.
 	Procfile map[string]string
 }
 
 func (b Build) String() string {
-	return b.Image
+	return b.Artifact
 }
