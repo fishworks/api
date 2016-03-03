2cc0a896e89d8fecf6a6253a3dc4270158fd2b4b
Matthew Fisher <mfisher@engineyard.com>
2016-03-03 01:33:45 -0800

    go fmt

--- config.go
+++ config.go
@@ -4,6 +4,6 @@ package api
 // in the execution environment. This also includes other values like the maximum allocated memory,
 // maximum cpu shares, isolating applications on a set of hosts via tags etc.
 type Config struct {
-	App         *App `json:"-"`
+	App         *App              `json:"-"`
 	Environment map[string]string `json:"environment"`
 }
